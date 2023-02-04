d45fbc5813c64ce5c96b75aa2c351e79bef31be2
José Leal <lealnetosena@gmail.com>
2023-02-04 07:45:32 -0300

    fim aula 16-01

--- routes/customers.js
+++ routes/customers.js
@@ -1,19 +1,33 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+
+// Conectando banco de dados
+const path = require("path");
+const { PATH_DB } = require("dotenv").config().parsed;
+const { Database } = require("sqlite3");
+
+const db = new Database(path.resolve(PATH_DB));
 
 // Criar um Cliente
-router.post("/", (req, res) => res.send("POST CLIENTE"))
+router.post("/", (req, res) => res.send("POST CLIENTE"));
 
 // Recuperar Todos os Clientes
-router.get("/", (req, res) => res.send("GET CLIENTE"))
+router.get("/", (req, res) => {
+  const sql = "SELECT * FROM customer";
+  console.log(
+    db.get(sql, function (err, data) {
+      console.log("arguments", err, data);
+      res.send(data);
+    })
+  );
+});
 
 // Recurear um Cliente Específico
 
-
 // Atualiza um Cliente
-router.put("/", (req, res) => res.send("PUT CLIENTE"))
+router.put("/", (req, res) => res.send("PUT CLIENTE"));
 
 // Remover um Cliente
-router.delete("/", (req, res) => res.send("DELETE CLIENTE"))
+router.delete("/", (req, res) => res.send("DELETE CLIENTE"));
 
 module.exports = router;
